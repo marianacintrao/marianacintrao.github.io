ac222a07bb4e8e2f3648db9a89029737212e6dfc
mariana cintrao <mariana.cintrao@gmail.com>
2020-06-17 19:52:49 +0100

    fixed yVel bug + adds new boid on click

--- boid.js
+++ boid.js
@@ -1,8 +1,8 @@
 const boidSize = 8;
 const maxSpeed = 2.5;
-const boidsMinDistance = 40;
+const boidsViewRadius = 40;
 const maxForce = 10;
-const mouseAffinityVal = 0.0; //should have decimal value
+const mouseAffinityVal = 0.1; //should have decimal value
 const separationRatio = 50;
 const alignmentRatio = 0.5;
 const cohesionRatio = 0.1;
@@ -23,17 +23,16 @@ class Boid {
     }
 
     draw(context) {
-        //context.strokeStyle = this.color;
         context.fillStyle = this.color;
         context.translate(this.xPos, this.yPos);
         context.rotate(this.angle + (Math.PI/2));
+
 
         context.beginPath();
         context.moveTo(0, 0);
         context.lineTo(boidSize/2, boidSize*2 );
         context.lineTo(-boidSize/2, boidSize*2);
         context.closePath();
-        //context.stroke();
         context.fill();
 
         context.rotate(-this.angle - (Math.PI/2));
@@ -45,15 +44,16 @@ class Boid {
 
         this.xVel += this.xAcc;
         this.yVel += this.yAcc;
-
+        
         this.angle = Math.atan(this.yVel/this.xVel);
         if (this.xVel < 0) this.angle += Math.PI;
 
-
-        this.xVel = maxSpeed * Math.cos(this.angle);
-
-        this.yVel = maxSpeed * Math.sin(this.angle);
-
+        var d = Math.sqrt(Math.pow(this.xVel, 2) + Math.pow(this.yVel, 2));
+        if (maxSpeed < d) {
+            this.xVel = maxSpeed * Math.cos(this.angle);
+            this.yVel = maxSpeed * Math.sin(this.angle);
+        }
+        
         this.xPos += this.xVel;
         this.yPos += this.yVel;
 
@@ -62,6 +62,8 @@ class Boid {
         if (this.xPos > window.innerWidth) this.xPos = 0;
         if (this.yPos > window.innerHeight) this.yPos = 0;
     }
+
+
 
     alignment_cohesion_separation(boids) {
         /* Alignment variables */
@@ -84,14 +86,15 @@ class Boid {
             let xDist = this.xPos - other.xPos;
             let yDist = this.yPos - other.yPos; 
             let distance = Math.sqrt(Math.pow(xDist, 2) +  Math.pow(yDist, 2));
-            if (other != this && distance < boidsMinDistance) {
+            if (other != this && distance < boidsViewRadius) {
 
                 xVelocities.push(other.xVel);
                 yVelocities.push(other.yVel);
 
+                
                 xPositions.push(other.xPos);
                 yPositions.push(other.yPos);
-
+                
                 xDistances.push(xDist);
                 yDistances.push(yDist);
             }
@@ -119,18 +122,23 @@ class Boid {
 
             this.xAcc = xAvgVel + xAvgPos + xAvgDist;
             this.yAcc = yAvgVel + yAvgPos + yAvgDist;
+
         }
     }
 
     mouseAffinity(x, y) {
         if (x != 0 && y!= 0) {
-            let directionX = x - this.xPos;
-            let directionY = y - this.yPos;
-            this.xAcc += (directionX * mouseAffinityVal);
-            this.yAcc += (directionY * mouseAffinityVal);
+            let directionX = this.xPos - x;
+            let directionY = this.yPos - y;
+            if (Math.sqrt(Math.pow(directionX, 2), Math.pow(directionY, 2)) < boidsViewRadius) {
+                this.xAcc += (directionX * mouseAffinityVal);
+                this.yAcc += (directionY * mouseAffinityVal);
+            }
         }
     }
 
     get x() { return this.xPos; }
     get y() { return this.yPos; }
+    get xV() { return this.xVel; }
+    get yV() { return this.yVel; }
 }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 const flock = [];
-var boidCounter = 100;
+var boidCounter;
 var x = 0;
 var y = 0;
 
@@ -11,12 +11,20 @@ let updateCoords = function(e) {
 function draw() {
     var canvas = document.getElementById("canvas");
     document.querySelector("canvas").addEventListener("mousemove", updateCoords);
+
+    canvas.onmousedown = function(e) {
+        console.log(boidCounter);
+        b = new Boid(e.clientX, e.clientY);
+        boidCounter++;
+        flock.push(b);
+    }
+    
     if (canvas.getContext) {
         var ctx = canvas.getContext("2d");
         canvas.width  = window.innerWidth;
         canvas.height = window.innerHeight;
+
         boidCounter = Math.floor(canvas.height * canvas.width / 8000);
-        console.log(boidCounter);
         
         for (var i = 0; i < boidCounter; i ++) {
             b = new Boid(Math.floor(Math.random()*canvas.width), Math.floor(Math.random()*canvas.height));
@@ -27,12 +35,10 @@ function draw() {
             requestAnimationFrame(update);
             ctx.fillStyle = "white";
             ctx.fillRect(0, 0, canvas.width, canvas.height);
-
             for (var i = 0; i < boidCounter; i ++) {
                 flock[i].alignment_cohesion_separation(flock);
                 flock[i].mouseAffinity(x, y);
                 flock[i].update(ctx);
-
             }
         }
         
